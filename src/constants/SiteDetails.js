28aacf71ac404da980babb745252f60f6c574df0
apple <apple@apples-MacBook-Pro.local>
2022-08-22 20:06:44 +0500

    changes done in ui

---
+++ src/constants/SiteDetails.js
@@ -0,0 +1,11 @@
+export default {
+	url: 'https://gallivantingsa.co.za',
+	app_key: 'f63c639b-63d0-4822-a79f-3323e607a3d5',
+	terms_page: 'https://gallivantingsa.co.za/faq/',
+}
+
+export const defaultLanguage = {
+	code: 'en',
+	rtl: false
+}
+export const defaultCurrency = 'USD';
